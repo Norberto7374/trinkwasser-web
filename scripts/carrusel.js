93fbfbdb74a1d4e75eae5377d1afbe503f080dc5
Norberto7374 <norberto_zamora@hotmail.com>
2026-03-22 20:07:36 -0600

    Implement carousel functionality for slides

--- scripts/carrusel.js
+++ scripts/carrusel.js
@@ -1,1 +1,12 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
 
+function showSlide() {
+  slides.forEach((s, i) => {
+    s.style.display = i === index ? "block" : "none";
+  });
+  index = (index + 1) % slides.length;
+}
+
+showSlide();
+setInterval(showSlide, 4000);
