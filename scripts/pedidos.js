be323222f42e4df8f70d87bcc97c53c42f3e1376
Norberto7374 <norberto_zamora@hotmail.com>
2026-03-23 19:54:28 -0600

    Update pedidos.js

--- scripts/pedidos.js
+++ scripts/pedidos.js
@@ -2,19 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const form = document.getElementById("pedidoForm");
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-        const nombre = document.querySelector("[name='nombre']").value;
-        const producto = document.querySelector("[name='producto']").value;
-        const cantidad = document.querySelector("[name='cantidad']").value;
+    const nombre = document.querySelector("[name='nombre']").value;
+    const direccion = document.querySelector("[name='direccion']").value;
+    const producto = document.querySelector("[name='producto']").value;
+    const cantidad = document.querySelector("[name='cantidad']").value;
+    const garrafones = document.querySelector("[name='garrafones']").value;
+    const tipo = document.querySelector("[name='tipo']").value;
+    const horario = document.querySelector("[name='horario']").value;
+    const pago = document.querySelector("[name='pago']").value;
 
-        const mensaje = `Hola, quiero realizar un pedido:%0A
+    const mensaje = `Hola, quiero realizar un pedido:%0A
+    
 Nombre: ${nombre}%0A
+Dirección: ${direccion}%0A
 Producto: ${producto}%0A
-Cantidad: ${cantidad}`;
+Cantidad: ${cantidad}%0A
+Garrafones: ${garrafones}%0A
+Tipo: ${tipo}%0A
+Horario: ${horario}%0A
+Pago: ${pago}`;
 
-        const telefono = "525554144400";
+    
+
+        const telefono = "525554356827";
 
         window.open(`https://wa.me/${telefono}?text=${mensaje}`, "_blank");
 
