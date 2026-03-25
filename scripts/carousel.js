c2392c1f98b5a29df2de6d8b9443335be7ffc31b
Norberto7374 <norberto_zamora@hotmail.com>
2026-03-25 10:57:45 -0600

    Update carousel.js

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -6,16 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let index = 0;
 
   function showSlide() {
-    slides.forEach((slide) => {
-      slide.style.display = "none";
-    });
+    slides.forEach(slide => slide.classList.remove("active"));
 
-    slides[index].style.display = "block";
+    slides[index].classList.add("active");
 
     index++;
-    if (index >= slides.length) {
-      index = 0;
-    }
+    if (index >= slides.length) index = 0;
   }
 
   showSlide();
